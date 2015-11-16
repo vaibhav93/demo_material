c56c6b08a15e9e19992fee78311185b3bca4158a
Vaibhav Bansal <vaibhav.b.bansal@oracle.com>
2015-11-16 17:14:44 +0530

    side menu knockout

--- js/app.js
+++ js/app.js
@@ -1,21 +1,84 @@
 define(['jquery', 'knockout', 'Materialize', 'pager', '../scripts/jquery.components'], function ($, ko, Materialize, pager) {
-    
+
     var viewModel = {
-        user:{
-            email:'vaibhav.b.bansal@oracle.com'
+        sideMenu: ko.observableArray(),
+        fetchSideMenu: function () {
+            $.getJSON("http://nameless-sun-2869.getsandbox.com/sidemenu", function (data) {
+                viewModel.sideMenu(data);
+            });
         },
-        demoText: ko.observable('Demo Text'),
-        topics:{
-            demoText2 : 'yoloo',
-            pageLoaded : function(){
+        user: {
+            email: 'vaibhav.b.bansal@oracle.com'
+        },
+        topics: {
+            pageLoaded: function () {
                 console.log('topics loaded');
                 window.topicPageComponents();
+
             }
+        },
+        applications: {
+            showNewAppForm: function () {
+                var appForm = $('#app-form')
+                if (appForm.is(":hidden")) {
+                    appForm.slideDown('slow')
+                } else {
+                    appForm.slideUp();
+                }
+            },
+            newApp: {
+                name: ko.observable(''),
+                desc: ko.observable('')
+            },
+            submitApp: function () {
+                console.log(viewModel.applications.newApp);
+                viewModel.applications.appsList.push({
+                    name: viewModel.applications.newApp.name(),
+                    desc: viewModel.applications.newApp.desc(),
+                    icon: 'assignment_ind',
+                    color: 'blue'
+                });
+                setTimeout(function () {
+                    viewModel.applications.newApp.name('');
+                    viewModel.applications.newApp.desc('');
+                    $('#app-form').slideUp();
+                }, 1000)
+
+            },
+            appsList: ko.observableArray([
+                {
+                    name: 'Aria People',
+                    desc: 'Oracle employee repository',
+                    icon: 'assignment_ind',
+                    color: 'blue'
+                },
+                {
+                    name: 'Oracle OIM',
+                    desc: 'Oracle Identity Management',
+                    icon: 'folder',
+                    color: ''
+                },
+                {
+                    name: 'Oracle GIS',
+                    desc: 'Oracle Global information security',
+                    icon: 'insert_chart',
+                    color: 'green'
+                },
+                {
+                    name: 'Oracle Taleo',
+                    desc: 'Oracle recruitment portal',
+                    icon: 'play_arrow',
+                    color: 'red'
+                }
+            ])
         }
     };
     pager.Href.hash = '#!/';
     // extend your view-model with pager.js specific data
     pager.extendWithPage(viewModel);
+
+    viewModel.fetchSideMenu();
+
     // apply the view-model using KnockoutJS as normal
     ko.applyBindings(viewModel);
     // start pager.js
